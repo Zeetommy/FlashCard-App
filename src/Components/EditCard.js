e109c991ddc393101faaf00f02d2f62007694b70
Thomas Vilca <thomasvilca1570@gmail.com>
2023-04-12 17:30:17 -0400

    Made EditCard component and started with useEffect and other functions

---
+++ src/Components/EditCard.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react"
+import { Link, useHistory, useParams } from "react-router-dom"
+import { readCard, readDeck, updateCard } from "../utils/api/index"
+
+function EditCard() {
+    const { deckId, cardId } = useParams();
+    const history = useHistory();
+    const initialDeckState = {
+        id: "",
+        name: "",
+        description: "",
+    };
+    const initialCardState = {
+        id: "",
+        front: "",
+        back: "",
+        deckId: "",
+    };
+
+    const [card, setCard] = useState(initialDeckState);
+    const [deck, setDeck] = useState(initialCardState);
+
+    useEffect(() => {
+        async function fetchData() {
+            const abortController = new AbortController();
+            try {
+                const cardResponse = await readCard(
+                    cardId,
+                    abortController.signal
+                );
+                const deckResponse = await readDeck(
+                    deckId,
+                    abortController.signal
+                );
+                setCard(cardResponse);
+                setDeck(deckResponse);
+            } catch (error) {
+                console.error("Something went wrong", error);
+            }
+            return () => {
+                abortController.abort();
+            };
+        }
+        fetchData();
+    }, []);
+
+    function handleChange({ target }) {
+        setCard({
+            ...card,
+            [target.name]: target.value,
+        });
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const abortController = new AbortController();
+        const response = await updateCard({ ...card }, abortController.signal);
+        history.push(`/decks/${deckId}`);
+        return response;
+    }
+
+    async function handleCancel() {
+        history.push(`/decks/${deckId}`);
+    }
+}
