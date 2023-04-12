d6300a6951db1c4ad18d94065660c81792eadb22
Thomas Vilca <thomasvilca1570@gmail.com>
2023-04-12 14:53:50 -0400

    Finished the Create Deck component

--- src/Layout/CreateDeck.js
+++ src/Layout/CreateDeck.js
@@ -10,4 +10,66 @@ function CreateDeck(){
     }
 
     const [newDeck, setNewDeck] = useState(initialState) 
+    const handleChange({target}){
+        setNewDeck({
+            ...newDeck,
+            [target.name]: target.value,
+        })
+    }
+
+    async function handleSubmit(event){
+        event.prevetDefault()
+        const abortController = new AbortController()
+        const response = await createDeck(
+            {...newDeck},
+            abortController.signal
+        )
+        history.push("/")
+        return response
+    }
+
+    async function handeCancel(){
+        history.push("/")
+    }
+
+    return (
+        <div>
+            <ol>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>Create Deck</li>
+            </ol>
+            <form onSubmit={(event)=>handleSubmit(event)}>
+                <h1>Create Deck</h1>
+                <div>
+                    <label>Name</label>
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        onChange={handleChange}
+                        value={newDeck.name}
+                    />
+                </div>
+                <div>
+                    <label>Description</label>
+                    <input 
+                        id="descripton"
+                        name="description"
+                        type="text"
+                        onChange={handleChange}
+                        value={newDeck.description}
+                    />
+                </div>
+                <button onClick={() => handleCancel()}>
+                    Cancel
+                </button>
+                <button type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
 }
+export default CreateDeck
