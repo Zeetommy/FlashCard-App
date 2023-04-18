dfa287b0de049c964550b54a25e864d16bbe1b22
Thomas Vilca <thomasvilca1570@gmail.com>
2023-04-18 17:15:22 -0400

    clean up

--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -1,150 +1,125 @@
-import React, { useState, useEffect } from "react"
-import { Link, useParams, useHistory } from "react-router-dom"
-import { readDeck, deleteDeck, deleteCard } from "../utils/api/index"
+import React, { useState, useEffect } from "react";
+import { Link, useParams, useHistory } from "react-router-dom";
+import { readDeck, deleteDeck, deleteCard } from "../utils/api/index";
 
 function Deck() {
-    const history = useHistory()
-    const { deckId } = useParams()
-    const [deck, setDeck] = useState({})
-    const [cards, setCards] = useState([])
+  const history = useHistory();
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState({});
+  const [cards, setCards] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-            const abortController = new AbortController()
-            try {
-                const deckResponse = await readDeck(
-                    deckId,
-                    abortController.signal
-                )
-                setDeck(deckResponse)
-                setCards(deckResponse.cards)
-            } catch (error) {
-                console.error("Something went wrong", error)
-            }
-            return () => {
-                abortController.abort()
-            }
-        }
-        fetchData()
-    }, [deckId])
-
-    async function handleDeleteDeck(deck) {
-      if (
-        window.confirm(`Delete this deck? You will not be able to recover it`)
-      ) {
-        const abortController = new AbortController()
-        try {
-          history.push("/")
-          return await deleteDeck(deck.id, abortController.signal)
-        } catch (error) {
-          console.error("Something went wrong", error)
-        }
-        return () => {
-          abortController.abort()
-        }
+  useEffect(() => {
+    async function fetchData() {
+      const abortController = new AbortController();
+      try {
+        const deckResponse = await readDeck(deckId, abortController.signal);
+        setDeck(deckResponse);
+        setCards(deckResponse.cards);
+      } catch (error) {
+        console.error("Something went wrong", error);
       }
+      return () => {
+        abortController.abort();
+      };
     }
+    fetchData();
+  }, [deckId]);
 
-    async function handleDeleteCard(card) {
-      if (
-        window.confirm(`Delete this card? You will not be able to recover it`)
-      ) {
-        const abortController = new AbortController()
-        try {
-          history.go(0)
-          return await deleteCard(card.id, abortController.signal)
-        } catch (error) {
-          console.error("Something went wrong", error)
-        }
-        return () => {
-          abortController.abort()
-        }
+  async function handleDeleteDeck(deck) {
+    if (
+      window.confirm(`Delete this deck? You will not be able to recover it`)
+    ) {
+      const abortController = new AbortController();
+      try {
+        history.push("/");
+        return await deleteDeck(deck.id, abortController.signal);
+      } catch (error) {
+        console.error("Something went wrong", error);
       }
+      return () => {
+        abortController.abort();
+      };
     }
+  }
 
-    async function handleEditDeck() {
-      history.push(`/decks/${deckId}/edit`)
+  async function handleDeleteCard(card) {
+    if (
+      window.confirm(`Delete this card? You will not be able to recover it`)
+    ) {
+      const abortController = new AbortController();
+      try {
+        history.go(0);
+        return await deleteCard(card.id, abortController.signal);
+      } catch (error) {
+        console.error("Something went wrong", error);
+      }
+      return () => {
+        abortController.abort();
+      };
     }
+  }
 
-    async function handleStudy() {
-      history.push(`/decks/${deckId}/study`)
-    }
+  async function handleEditDeck() {
+    history.push(`/decks/${deckId}/edit`);
+  }
 
-    async function handleAddCard() {
-      history.push(`/decks/${deckId}/cards/new`)
-    }
+  async function handleStudy() {
+    history.push(`/decks/${deckId}/study`);
+  }
 
-    async function handleEditCard(card) {
-      history.push(`/decks/${deckId}/cards/${card.id}/edit`)
-    }
+  async function handleAddCard() {
+    history.push(`/decks/${deckId}/cards/new`);
+  }
 
-    if (cards.length > 0) {
-      return (
+  async function handleEditCard(card) {
+    history.push(`/decks/${deckId}/cards/${card.id}/edit`);
+  }
+
+  if (cards.length > 0) {
+    return (
+      <div>
+        <ol>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>{deck.name}</li>
+        </ol>
         <div>
-          <ol>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>{deck.name}</li>
-          </ol>
           <div>
-            <div>
-              <h2>{deck.name}</h2>
-              <p>{deck.description}</p>
-              <button
-                onClick={() => handleEditDeck()}
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => handleStudy()}
-              >
-                Study
-              </button>
-              <button
-                onClick={() => handleAddCard()}
-              >
-                Add Cards
-              </button>
-              <button
-                onClick={() => handleDeleteDeck(deck)}
-              >
-                Delete
-              </button>
-            </div>
+            <h2>{deck.name}</h2>
+            <p>{deck.description}</p>
+            <button onClick={() => handleEditDeck()}>Edit</button>
+            <button onClick={() => handleStudy()}>Study</button>
+            <button onClick={() => handleAddCard()}>Add Cards</button>
+            <button onClick={() => handleDeleteDeck(deck)}>Delete</button>
           </div>
-          <h1>Cards</h1>
-          {cards.map((card) => {
-            return (
-              <div key={card.id}>
+        </div>
+        <h1>Cards</h1>
+        {cards.map((card) => {
+          return (
+            <div key={card.id}>
+              <div>
                 <div>
-                  <div>
-                    <div className="row">
-                      <div className="col">{card.front}</div>
-                      <div className="col">{card.back}</div>
-                    </div>
-                    <div className="container row">
-                      <button
-                        onClick={() => handleEditCard(card)}
-                      >
-                        Edit
-                      </button>
-                      <button
-                        onClick={() => handleDeleteCard(card)}
-                      >
-                        Delete
-                      </button>
-                    </div>
+                  <div className="row">
+                    <div className="col">{card.front}</div>
+                    <div className="col">{card.back}</div>
+                  </div>
+                  <div className="container row">
+                    <button onClick={() => handleEditCard(card)}>Edit</button>
+                    <button onClick={() => handleDeleteCard(card)}>
+                      Delete
+                    </button>
                   </div>
                 </div>
               </div>
-            )
-          })}
-        </div>
-      )
-    } else {
-      return null
-    }
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
 
-export default Deck
+export default Deck;
